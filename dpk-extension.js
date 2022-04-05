0abb4a3e3b7f66bb36caacc9ff1db932fee4c9a2
ooiuri <oiuri@outlook.com>
2022-04-04 21:59:20 -0300

    Validando função fChecaCGC

--- dpk-extension.js
+++ dpk-extension.js
@@ -15,10 +15,41 @@ function changeParentFrame() {
     }
 }
 
+function fChecaCGC() {
+    var CGCCPF ='';
+    if(document.frmValidaCGC.txtCGC.value==''){
+        alert('Digite o CGC');
+        document.frmValidaCGC.txtCGC.focus();
+        return false;
+    }
+    else{
+        CGCCPF = document.frmValidaCGC.txtCGC.value;
+        if (CGCCPF.length<12){
+            if (isCPFCNPJ(document.frmValidaCGC.txtCGC,1)==true){
+                window.location.href = 'ValidaCGC.asp?action=2&vCGC='+document.frmValidaCGC.txtCGC.value;
+            }
+        }
+        else {
+            if (isCPFCNPJ(document.frmValidaCGC.txtCGC,2)==true){
+                window.location.href = 'ValidaCGC.asp?action=2&vCGC='+document.frmValidaCGC.txtCGC.value;
+            }
+        }
+    }
+}
+
 function embed(fn) {
     const script = document.createElement("script");
     script.text = `(${fn.toString()})();`;
+    
+    document.documentElement.appendChild(script);
+}
+
+function embedfChecaCGC(fn) {
+    const script = document.createElement("script");
+    script.text = `${fn.toString()}`;
+
     document.documentElement.appendChild(script);
 }
 
 embed(changeParentFrame);
+embedfChecaCGC(fChecaCGC);
